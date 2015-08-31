58c799f534f86e95b4dfe31ee0d6205164e18d71
JP Saraceno <jsarac@gmail.com>
2015-08-30 23:19:46 -0300

    Introduce redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import './styles.styl';
 import React from 'react';
 import App from './App';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
+import { devTools, persistState } from 'redux-devtools';
 import { Provider } from 'react-redux';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import { tasks } from './reducers';
 
+const finalCreateStore = compose(
+  // Provides support for DevTools:
+  devTools(),
+  // Lets you write ?debug_session=<name> in address bar to persist debug sessions
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+  createStore
+);
+
 let root = document.getElementById('app');
-let store = createStore(tasks);
+let store = finalCreateStore(tasks);
 
 React.render(
-  <Provider store={store}>
+  <div>
+    <Provider store={store}>
       { () => <App /> }
-  </Provider>,
+    </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>,
   root);
