c8d092a01ba302f4d7716d65dfa66c7dc8b01f30
JP Saraceno <jsarac@gmail.com>
2015-09-04 01:32:05 -0300

    move babel polyfill to the right place

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import TaskList from './TaskList';
 import { Statuses } from "./constants";
-import 'babel-core/polyfill';
 import { connect } from 'react-redux';
 
 export default class App extends React.Component {
