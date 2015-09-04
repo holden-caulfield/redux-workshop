644442c454e00e1254e0bd825763e74be9be1b1c
JP Saraceno <jsarac@gmail.com>
2015-09-03 22:54:53 -0300

    solve exercise 6

--- src/index.js
+++ src/index.js
@@ -3,17 +3,28 @@ import React from 'react';
 import App from './App';
 import Help from './Help';
 import * as reducers from './reducers';
-import { createStore, combineReducers } from 'redux';
+import { compose, createStore, combineReducers } from 'redux';
+import { devTools, persistState } from 'redux-devtools';
 import { Provider } from 'react-redux';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
+const finalCreateStore = compose(
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+  createStore
+);
 
 let root = document.getElementById('app');
-let store = createStore(combineReducers(reducers));
+let store = finalCreateStore(combineReducers(reducers));
 
 React.render(
   <div>
     <Provider store={store}>
       { () => <App /> }
     </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
     <Help />
   </div>,
   root);
